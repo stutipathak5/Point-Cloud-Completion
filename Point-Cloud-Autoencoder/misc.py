41a85e4c0de3789347e4287166bccb9c2a8d50d3
stutipathak5 <stutipathak.97@gmail.com>
2024-10-07 13:43:57 +0200

    benchmark conversion added

--- Point-Cloud-Autoencoder/misc.py
+++ Point-Cloud-Autoencoder/misc.py
@@ -1,5 +1,6 @@
 import numpy as np
 import h5py
+import open3d as o3d
 
 # pc_array = np.load("data/chair_set.npy")
 
@@ -44,86 +45,94 @@ import h5py
 
 
 
-import h5py
-import os
+# import h5py
+# import os
 
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import open3d as o3d
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
+# import open3d as o3d
 
-def load_h5(file_path, dataset_name):
-    with h5py.File(file_path, 'r') as hf:
-        array = hf[dataset_name][:]
-    return array
+# def load_h5(file_path, dataset_name):
+#     with h5py.File(file_path, 'r') as hf:
+#         array = hf[dataset_name][:]
+#     return array
 
-npy_folder_path = '../data/Dutch/easy_0.8'
+# npy_folder_path = '../data/Dutch/easy_0.8'
 
-def sample(point_cloud_array):
-    new_array = np.zeros((point_cloud_array.shape[0], 1024, 3))
-    for i in range(point_cloud_array.shape[0]):
-        indices = np.random.choice(point_cloud_array[i].shape[0], 1024, replace=False)
-        new_array[i] = point_cloud_array[i][indices, :]
-    return new_array
+# def sample(point_cloud_array):
+#     new_array = np.zeros((point_cloud_array.shape[0], 1024, 3))
+#     for i in range(point_cloud_array.shape[0]):
+#         indices = np.random.choice(point_cloud_array[i].shape[0], 1024, replace=False)
+#         new_array[i] = point_cloud_array[i][indices, :]
+#     return new_array
 
-complete = sample(load_h5(os.path.join(npy_folder_path, "complete.h5"), "complete"))
-occl = sample(load_h5(os.path.join(npy_folder_path, "occl.h5"), "occl"))
-non_sparse = sample(load_h5(os.path.join(npy_folder_path, "non_sparse.h5"), "non_sparse"))
-uni_sparse = sample(load_h5(os.path.join(npy_folder_path, "uni_sparse.h5"), "uni_sparse"))
+# complete = sample(load_h5(os.path.join(npy_folder_path, "complete.h5"), "complete"))
+# occl = sample(load_h5(os.path.join(npy_folder_path, "occl.h5"), "occl"))
+# non_sparse = sample(load_h5(os.path.join(npy_folder_path, "non_sparse.h5"), "non_sparse"))
+# uni_sparse = sample(load_h5(os.path.join(npy_folder_path, "uni_sparse.h5"), "uni_sparse"))
 
-print(complete.shape)
-print(occl.shape)
-print(non_sparse.shape)
-print(uni_sparse.shape)
-
-
-concatenated_array = np.concatenate((complete, occl, non_sparse, uni_sparse), axis=0)
+# print(complete.shape)
+# print(occl.shape)
+# print(non_sparse.shape)
+# print(uni_sparse.shape)
 
 
-def normalize(pc_array):
-    for i in range(pc_array.shape[0]):
-        ptcloud = pc_array[i]
-        pcd = o3d.geometry.PointCloud()
-        pcd.points = o3d.utility.Vector3dVector(ptcloud)
-        bbox_pcd = pcd.get_axis_aligned_bounding_box()
-        corners = bbox_pcd.get_box_points()
-        bbox = np.asarray(corners)
-        bbox[[3, 7]] = bbox[[7, 3]]
-        center = (bbox.min(0) + bbox.max(0)) / 2
-        bbox -= center
-        yaw = np.arctan2(bbox[3, 1] - bbox[0, 1], bbox[3, 0] - bbox[0, 0])
-        rotation = np.array([[np.cos(yaw), -np.sin(yaw), 0], [np.sin(yaw), np.cos(yaw), 0], [0, 0, 1]])
-        bbox = np.dot(bbox, rotation)
-        scale = bbox[3, 0] - bbox[0, 0]
-        bbox /= scale
-        ptcloud = np.dot(ptcloud - center, rotation) / scale
-        ptcloud = np.dot(ptcloud, [[1, 0, 0], [0, 0, 1], [0, 1, 0]])
-        pc_array[i] = ptcloud
-    return pc_array
+# concatenated_array = np.concatenate((complete, occl, non_sparse, uni_sparse), axis=0)
 
 
-concatenated_array = normalize(concatenated_array)
-print(concatenated_array.shape)
+# def normalize(pc_array):
+#     for i in range(pc_array.shape[0]):
+#         ptcloud = pc_array[i]
+#         pcd = o3d.geometry.PointCloud()
+#         pcd.points = o3d.utility.Vector3dVector(ptcloud)
+#         bbox_pcd = pcd.get_axis_aligned_bounding_box()
+#         corners = bbox_pcd.get_box_points()
+#         bbox = np.asarray(corners)
+#         bbox[[3, 7]] = bbox[[7, 3]]
+#         center = (bbox.min(0) + bbox.max(0)) / 2
+#         bbox -= center
+#         yaw = np.arctan2(bbox[3, 1] - bbox[0, 1], bbox[3, 0] - bbox[0, 0])
+#         rotation = np.array([[np.cos(yaw), -np.sin(yaw), 0], [np.sin(yaw), np.cos(yaw), 0], [0, 0, 1]])
+#         bbox = np.dot(bbox, rotation)
+#         scale = bbox[3, 0] - bbox[0, 0]
+#         bbox /= scale
+#         ptcloud = np.dot(ptcloud - center, rotation) / scale
+#         ptcloud = np.dot(ptcloud, [[1, 0, 0], [0, 0, 1], [0, 1, 0]])
+#         pc_array[i] = ptcloud
+#     return pc_array
 
-# Create a 3D plot
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
 
-# Plot each point cloud on the same 3D plot
-for i in range(3):
-    point_cloud = concatenated_array[i]
-    ax.scatter(point_cloud[:, 0], point_cloud[:, 1], point_cloud[:, 2], label=f'Point Cloud {i+1}', s=1)
+# concatenated_array = normalize(concatenated_array)
+# print(concatenated_array.shape)
 
-# Set labels for the axes
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
+# # Create a 3D plot
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
 
-# Add a legend
-ax.legend()
+# # Plot each point cloud on the same 3D plot
+# for i in range(3):
+#     point_cloud = concatenated_array[i]
+#     ax.scatter(point_cloud[:, 0], point_cloud[:, 1], point_cloud[:, 2], label=f'Point Cloud {i+1}', s=1)
 
-# Show the plot
-plt.show()
+# # Set labels for the axes
+# ax.set_xlabel('X')
+# ax.set_ylabel('Y')
+# ax.set_zlabel('Z')
+
+# # Add a legend
+# ax.legend()
+
+# # Show the plot
+# plt.show()
+
+
+x = np.load(r"C:\Users\spathak\Downloads\PCC\Code\Point Cloud Completion\Point-Cloud-Autoencoder\data\final_splits\Dutch\easy\splits\part_tr.npy")
+
+print(x.shape)
+
+
+
 
 
 
