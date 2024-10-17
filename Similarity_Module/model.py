0b883aa2cf50609ec316d302852c0f2d9379b8d6
stutipathak5 <stutipathak.97@gmail.com>
2024-10-17 14:56:09 +0200

    updates

---
+++ Similarity_Module/model.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+'''
+PointNet AutoEncoder
+Learning Representations and Generative Models For 3D Point Clouds
+https://arxiv.org/abs/1707.02392
+'''
+
+class PointCloudAE(nn.Module):
+    def __init__(self, input_size, output_size, latent_size):
+        super(PointCloudAE, self).__init__()
+        
+        self.latent_size = latent_size
+        self.input_size = input_size
+        self.output_size = output_size
+        
+        self.conv1 = torch.nn.Conv1d(3, 64, 1)
+        self.conv2 = torch.nn.Conv1d(64, 128, 1)
+        self.conv3 = torch.nn.Conv1d(128, 256, 1)
+        self.conv4 = torch.nn.Conv1d(256, 512, 1)
+        self.conv5 = torch.nn.Conv1d(512, self.latent_size, 1)
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(256)
+        self.bn4 = nn.BatchNorm1d(512)
+        self.bn5 = nn.BatchNorm1d(self.latent_size)
+        
+        self.dec1 = nn.Linear(self.latent_size,512)
+        self.dec2 = nn.Linear(512,512)
+        self.dec3 = nn.Linear(512,1024)
+        
+        self.dec4 = nn.Linear(1024,self.output_size*3)
+
+    def encoder(self, x): 
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = self.bn5(self.conv5(x))
+        x = torch.max(x, 2, keepdim=True)[0]
+        x = x.view(-1, self.latent_size)
+        return x
+    
+    def decoder(self, x):
+        x = F.relu(self.dec1(x))
+        x = F.relu(self.dec2(x))
+        x = F.relu(self.dec3(x))
+        x = self.dec4(x)
+        return x.view(-1, self.output_size, 3)
+    
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+    
